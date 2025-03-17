8d53772b1d017376e4db671372d323a53945b1c6
MrMejor <jashanj2001@gmail.com>
2025-03-18 00:48:04 +0100

    Tried to host my website

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
+  // apiUrl: "https://9c95-2a0c-5a83-f605-3600-1114-8738-963b-6d8c.ngrok-free.app",
   tokenSecure: false
 }
